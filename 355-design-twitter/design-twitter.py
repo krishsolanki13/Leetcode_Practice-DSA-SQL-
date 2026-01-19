87781dcface1ec9fc9941cba0fc79a735170e496
Krish Solanki <166121243+krishsolanki13@users.noreply.github.com>
2026-01-19 22:08:37 +0530

    Time: 17 ms (41.10%) | Memory: 28.8 MB (7.18%) - LeetSync

--- 355-design-twitter/design-twitter.py
+++ 355-design-twitter/design-twitter.py
@@ -1,35 +1,38 @@
 class Twitter:
 
     def __init__(self):
-        self.count = 0
-        # Map each user and the tweets they have made
-        self.tweetMap = defaultdict(list) #UserID -> [count, tweetID]
-        # Map each user and who they follow
-        self.followMap = defaultdict(set) #UserID -> set(followeeID)
+        self.count = 0 #Tracks the tweet in time
+        self.tweetMap = defaultdict(list) # UserId -> [count, tweetId]
+        self.followMap = defaultdict(set) # UserId -> set(userIds)
 
     def postTweet(self, userId: int, tweetId: int) -> None:
         self.tweetMap[userId].append([self.count, tweetId])
         self.count -= 1
-
+ 
     def getNewsFeed(self, userId: int) -> List[int]:
         res = []
         minheap = []
-
         self.followMap[userId].add(userId)
+
+        #Initialise the heap with the recent most tweets of the followees
         for followeeId in self.followMap[userId]:
             if followeeId in self.tweetMap:
-                index = len(self.tweetMap[followeeId]) - 1
-                count, tweetId = self.tweetMap[followeeId][index]
-                minheap.append([count, tweetId, followeeId, index-1])
-        # print(minheap)
+                lastindex = len(self.tweetMap[followeeId]) - 1
+                count, tweetId = self.tweetMap[followeeId][lastindex]
+                minheap.append([count, tweetId, followeeId, lastindex - 1])
+        
         heapq.heapify(minheap)
+        # Heap initialised with the last tweets of the followees
+        # Now we populate the res until either res gets full or heap gets empty
 
         while minheap and len(res) < 10:
-            count, tweetId, followeeId, index = heapq.heappop(minheap)
+            # Pop from the heap and append to res the most recent tweetID
+            count, tweetId, followeeId, lastindex = heapq.heappop(minheap)
             res.append(tweetId)
-            if index >= 0:
-                count, tweetId = self.tweetMap[followeeId][index]
-                heapq.heappush(minheap, [count, tweetId, followeeId, index-1])
+            # Push to the heap next recent tweet for the same followee
+            if lastindex >= 0:
+                count, tweetId = self.tweetMap[followeeId][lastindex]
+                heapq.heappush(minheap, [count, tweetId, followeeId, lastindex - 1])
         
         return res
 
@@ -39,3 +42,4 @@ class Twitter:
     def unfollow(self, followerId: int, followeeId: int) -> None:
         if followeeId in self.followMap[followerId]:
             self.followMap[followerId].remove(followeeId)
+
